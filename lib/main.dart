962dacf21fc1fb2276ac8b7b8fdf979a16bcd77c
nobu74658 <hiroki-pon@outlook.jp>
2025-05-04 16:37:22 +0900

    feat: todoタイル作成

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ,
+      home: TodoListScreen(),
     );
+  }
+}
+
+class TodoListScreen extends StatelessWidget {
+  const TodoListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo App'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const CircleAvatar(
+              radius: 4,
+              backgroundColor: Colors.green,
+            ),
+            title: const Text('sample todo task'),
+            subtitle: const Text('18:00'),
+            trailing: Checkbox(
+              value: false,
+              onChanged: (value) {},
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _CustomText extends StatelessWidget {
+  const _CustomText({super.key, required this.index});
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    print('build: $index');
+    return const Text('設定項目');
   }
 }
